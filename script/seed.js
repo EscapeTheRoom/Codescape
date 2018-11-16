55a194bc8667304d211133133b23ae61febd9672
Anna Kaplan <annakaplanart@gmail.com>
2018-11-16 12:47:55 -0500

    feature-seed-dummy data in seed file

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,8 @@
 'use strict'
-
+const fs = require('fs')
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Problem} = require('../server/db/models')
+const spec1File = fs.readFileSync(__dirname + '/specFiles/spec1.js' )
 
 async function seed() {
   await db.sync({force: true})
@@ -11,10 +12,14 @@ async function seed() {
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
-
+  const problems = await Promise.all([
+    Problem.create({title: 'spec1', prompt:"lala spec 1\nhey", spec: spec1File})
+  ])
+  
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
+
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
---
+++ script/specFiles/spec1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiThings = require('chai-things');
+
+ describe('`getInitials` utility method', () => {
+    it('takes a string and returns a string', () => {
+      const initials = utils.getInitials('Corey Greenwald');
+      expect(initials).to.be.a('string');
+    });
+
+    it('returns the first letter of each word in the input string, capitalized', () => {
+      const initialsGHA = utils.getInitials('Grace Hopper Academy');
+      expect(initialsGHA).to.equal('GHA');
+      const initialsHATEOAS = utils.getInitials('hypermedia as the engine of application state');
+      expect(initialsHATEOAS).to.equal('HATEOAS');
+    });
+  });
+
