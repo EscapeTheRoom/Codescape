0059430b2c8706936f94293ba415e37407af9639
Anna Kaplan <annakaplanart@gmail.com>
2018-11-19 12:02:59 -0500

    feature-room level1 - made object in room to have click handler to popup input component

---
+++ client/components/rooms/level1.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { throws } from 'assert';
+
+class Level1 extends Component { 
+    constructor(){
+        super()
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(e){
+        console.log("event Target", e.target.id)
+        this.props.history.push(`/problem/${e.target.id}`)
+    }
+    render(){
+        return(
+            <div onClick={this.handleClick} >
+                <img  id={1} src="https://www.freeiconspng.com/uploads/beds-bedroom-icon-25.png"></img>
+            </div>
+        )
+    }
+
+}
+export default Level1;
