ecba97ee0d057ac4405e3d2ad5f558eb0bfe38c6
Connie Lim <jlim13@binghamton.edu>
2018-11-27 12:25:23 -0500

    fixed level2 isSolved and handleWin

--- client/store/level2guest.js
+++ client/store/level2guest.js
@@ -5,15 +5,15 @@ import history from '../history'
  * ACTION TYPES
  */
 
-const GAME_WON = 'GAME_WON'
-const SOLVED = 'SOLVED'
-const GET_ITEM = 'GET_ITEM'
-const RESET = 'RESET'
+const GAME_WON2 = 'GAME_WON2'
+const SOLVED2 = 'SOLVED2'
+const GET_ITEM2 = 'GET_ITEM2'
+const RESET2 = 'RESET2'
 
 /**
  * INITIAL STATE
  */
-const defaultGuest = {
+const defaultGuest2 = {
   isWon2: false,
   isSolved2: {
     4: 'false',
@@ -34,27 +34,27 @@ const defaultGuest = {
  * ACTION CREATORS
  */
 
-const gameIsWonGuest = () => ({type: GAME_WON})
-const problemSolvedGuest = problemId => ({type: SOLVED, problemId})
-const getItemTruthy = problemId => ({type: GET_ITEM, problemId})
-const resetGame = () => ({type: RESET})
+const gameIsWonGuest2 = () => ({type: GAME_WON2})
+const problemSolvedGuest2 = problemId => ({type: SOLVED2, problemId})
+const getItemTruthy2 = problemId => ({type: GET_ITEM2, problemId})
+const resetGame2 = () => ({type: RESET2})
 
 /**
  * THUNK CREATORS
  */
 
 export const guestGameWon2 = () => dispatch => {
-  dispatch(gameIsWonGuest())
+  dispatch(gameIsWonGuest2())
 }
 export const guestIsSolved2 = problemId => dispatch => {
-  dispatch(problemSolvedGuest(problemId))
+  dispatch(problemSolvedGuest2(problemId))
 }
 
 export const getItemSolved2 = problemId => dispatch => {
-  dispatch(getItemTruthy(problemId))
+  dispatch(getItemTruthy2(problemId))
 }
 export const resetState2 = () => dispatch => {
-  dispatch(resetGame())
+  dispatch(resetGame2())
 }
 // export const guestUpdateIsSolved = isSolvedObj => async dispatch => {
 //   try {
@@ -69,33 +69,33 @@ export const resetState2 = () => dispatch => {
 /**
  * REDUCER
  */
-export default function(state = defaultGuest, action) {
+export default function(state = defaultGuest2, action) {
   switch (action.type) {
-    case GAME_WON:
+    case GAME_WON2:
       for (let key in state.isSolved2) {
         if (state.isSolved2[key] === 'false') {
           return {...state}
         }
         return {...state, isWon2: true}
       }
-    case SOLVED:
-      let newState = {...state}
-      const newIsSolved = {...newState.isSolved2}
-      newIsSolved[action.problemId] = 'true'
-      const newItemIsUnlocked = {...newState.items2}
+    case SOLVED2:
+      let newState2 = {...state}
+      const newIsSolved2 = {...newState2.isSolved2}
+      newIsSolved2[action.problemId] = 'true'
+      const newItemIsUnlocked2 = {...newState2.items2}
       if (+action.problemId < 7) {
-        newItemIsUnlocked[+action.problemId + 1] = 'true'
-        return {...newState, isSolved2: newIsSolved, items2: newItemIsUnlocked}
+        newItemIsUnlocked2[+action.problemId + 1] = 'true'
+        return {...newState2, isSolved2: newIsSolved2, items2: newItemIsUnlocked2}
       } else {
-        return {...newState, isSolved2: newIsSolved}
+        return {...newState2, isSolved2: newIsSolved2}
       }
-    case GET_ITEM:
-      let newerState = {...state}
-      const newItemIsSolved = {...newerState.items2}
-      newItemIsSolved[action.problemId] = 'true'
-      return {...newerState, items2: newItemIsSolved}
-    case RESET:
-      return defaultGuest
+    case GET_ITEM2:
+      let newerState2 = {...state}
+      const newItemIsSolved2 = {...newerState2.items2}
+      newItemIsSolved2[action.problemId] = 'true'
+      return {...newerState2, items2: newItemIsSolved2}
+    case RESET2:
+      return defaultGuest2
     default:
       return state
   }
